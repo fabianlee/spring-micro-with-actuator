28eec3a9a903b48d3483cd025884822666f0610a
flee999 <flee999@hotmail.com>
2022-06-08 21:00:53 -0400

    new user service

--- src/main/java/org/fabianlee/springmicrowithactuator/user/UserController.java
+++ src/main/java/org/fabianlee/springmicrowithactuator/user/UserController.java
@@ -1,20 +1,10 @@
 package org.fabianlee.springmicrowithactuator.user;
 
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +22,8 @@ public class UserController {
     @ApiResponse(responseCode = "200", description = "list returned")
     @GetMapping
     public Iterable<User> findAllUsers() {
-        return ArrayList<User> = new ArrayList<User>({new User("moe"),new User("curly")});
+        Iterable<User> list = Arrays.asList(new User("moe"),new User("larry"));
+        return list;
     }
 
 
