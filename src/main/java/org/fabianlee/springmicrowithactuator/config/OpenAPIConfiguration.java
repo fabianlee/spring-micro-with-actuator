b0fb3ac290b55d398b139cb9dd8ac0fb0365dc65
flee999 <flee999@hotmail.com>
2022-06-08 20:09:26 -0400

    testing openapi customizatin

---
+++ src/main/java/org/fabianlee/springmicrowithactuator/config/OpenAPIConfiguration.java
@@ -0,0 +1,21 @@
+/**
+ * Seeing if we can change the context path called by the OpenAPI swagger page
+ * when we are using external ingress to change the path
+ */
+package org.fabianlee.springmicrowithactuator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/api/v1"));
+    }
+}
